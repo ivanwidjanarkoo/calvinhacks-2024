affbf29b01c390131dd8ff648ff83a59eae2928d
= <iw3@calvin.edu>
2024-03-09 10:30:27 -0500

    first

--- src/components/SponsorSection/index.js
+++ src/components/SponsorSection/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { SponsorContainer, SponsorImageContainer, SponsorSilver, SponsorBronze, SponsorGold, SponsorLink } from './SponsorElement';
-import dornerworks from '../../image/dornerworks.png';
-import spindance from '../../image/spindance.jpg';
-import atomicobject from '../../image/atomic.png';
+//import dornerworks from '../../image/dornerworks.png';
+//import spindance from '../../image/spindance.jpg';
+//import atomicobject from '../../image/atomic.png';
 import tekton from '../../image/tekton.png';
-import twisthink from '../../image/twisthink.png';
-import calvin from '../../image/calvin.jpg'
+//import twisthink from '../../image/twisthink.png';
+//import calvin from '../../image/calvin.jpg'
 
 const SponsorSection = () => {
     return (
@@ -18,17 +18,17 @@ const SponsorSection = () => {
                     </SponsorLink>
                 </SponsorGold>
                 <SponsorSilver>
-                    <SponsorLink href='https://dornerworks.com/'>
+                    {/* <SponsorLink href='https://dornerworks.com/'>
                         <img src={dornerworks} alt='dornerworks' />
-                    </SponsorLink>
+                    </SponsorLink> */}
                 </SponsorSilver>
                 <SponsorBronze>
                     {/*<SponsorLink href='https://spindance.com/'>
                         <img src={spindance} alt='spindance' />
     </SponsorLink>*/}
-                    <SponsorLink href='https://twisthink.com/'>
+                    {/* <SponsorLink href='https://twisthink.com/'>
                         <img src={twisthink} alt='twisthink' />
-                    </SponsorLink>
+                    </SponsorLink> */}
                     {/*<SponsorLink href='https://calvin.edu/'>
                         <img src={calvin} alt='calvinuniversity' />
     </SponsorLink>*/}
